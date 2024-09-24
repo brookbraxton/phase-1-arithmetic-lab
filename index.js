b28750aa8c61e3bfd955a69d12681f41c8568699
roselyne mwaniki <roselynewairimu30@gmail.com>
2024-09-24 15:04:31 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
-// Write your code here
+let num1 =31
+let num2 =2
+let multiply = (num1 * num2)
+ console.log(multiply)
+ let random = 4
+ console.log(random)
+ let num3 = 12
+ let num4 =8
+ let mod = num3 % num4
+ console.log(mod);
+ 
+ 
+const max = 20
+console.log(max)
